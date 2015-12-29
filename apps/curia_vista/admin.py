79888c8c195395a0899554f38943ca68c0d9417a
shuegli <sebastianhuegli@gmail.com>
2015-12-29 15:27:33 +0100

    admin.py: classes registered

--- apps/curia_vista/admin.py
+++ apps/curia_vista/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+
 from apps.curia_vista.models import *
 
 admin.site.register(Council)
 admin.site.register(Councillor)
+admin.site.register(Schedule)
+admin.site.register(Affair)
+admin.site.register(AffairSummary)
+admin.site.register(Committee)
+admin.site.register(LegislativePeriod)
+admin.site.register(Department)
+admin.site.register(Canton)
+admin.site.register(Faction)
+admin.site.register(Session)
+admin.site.register(Party)
+admin.site.register(Vote)
