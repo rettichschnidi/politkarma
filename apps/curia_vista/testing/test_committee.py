1d81015126458ed24d4cedb44507b89014f0c35c
Reto Schneider <code@reto-schneider.ch>
2016-01-01 23:45:51 +0100

    Update/fix test for Committee

--- apps/curia_vista/testing/test_committee.py
+++ apps/curia_vista/testing/test_committee.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 
-from apps.curia_vista.models import Committee
+from apps.curia_vista.models import Committee, Council
 
 
 class TestFaction(TestCase):
     def setUp(self):
-        self.T = Committee(id=1, updated='2010-12-26T13:06:13Z', abbreviation='Bü-NR', code='KOM_1_',
-                           committee_number='1', from_date='1848-11-07T00:00:00Z', is_active=True, name='Büro NR',
-                           type_code='1')
+        self.council = Council(id=1, updated='2010-12-26T13:07:49Z', abbreviation='NR', code='RAT_1_', type='N',
+                               name='Nationalrat')
+        self.committee = Committee(id=1, updated='2010-12-26T13:06:13Z', abbreviation='Bü-NR', code='KOM_1_', number=1,
+                                   council=self.council, from_date='1848-11-07T00:00:00Z', is_active=True,
+                                   name='Büro NR', type_code='1')
+        self.T = self.committee
 
     def test___str__(self):
         self.assertEqual('Büro NR', str(self.T))
