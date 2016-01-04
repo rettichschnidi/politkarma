f8de7130773a5e3dd6f9ca5aa9dce477ddb09ff7
shuegli <sebastianhuegli@gmail.com>
2016-01-04 20:32:56 +0100

    example for view class added

--- apps/curia_vista/urls.py
+++ apps/curia_vista/urls.py
@@ -1,10 +1,11 @@
-from django.views.generic import DetailView
-from django.views.generic.list import ListView
 from django.conf.urls import url
+from django.views.generic.list import ListView
 
 from apps.curia_vista.models import Department
+from apps.curia_vista.views import *
 
 urlpatterns = [
     url(r'^department/$', ListView.as_view(context_object_name='departments',
                                            queryset=Department.objects.order_by('-name'))),
+    url(r'^party/$', PartyView.as_view(), name='party'),
 ]
--- apps/curia_vista/views.py
+++ apps/curia_vista/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from apps.curia_vista.models import Party
+
 
 # Create your views here.
+class PartyView(TemplateView):
+    template_name = 'curia_vista/party_list.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {'parties': Party.objects.order_by('name')}
+        return self.render_to_response(context)
