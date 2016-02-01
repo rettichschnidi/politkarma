3a693efd9039515cef4951ea7244f0b25685aad1
shuegli <sebastianhuegli@gmail.com>
2016-02-01 23:23:20 +0100

    model class updated

--- apps/curia_vista/models.py
+++ apps/curia_vista/models.py
@@ -176,7 +176,7 @@ class AffairRole(models.Model):
 class AffairText(models.Model):
     id = models.AutoField(primary_key=True)
     type = models.ForeignKey('AffairTextType')
-    value = models.CharField(max_length=1024)
+    value = models.CharField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.id
