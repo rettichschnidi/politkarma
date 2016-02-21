a2ca17316c8911719a2d0cd11cda707ba7ee8b57
Reto Schneider <code@reto-schneider.ch>
2016-02-21 02:29:36 +0100

    Fail the Travis CI build on the first error

---
+++ travis/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#Exit on first error
+set -e
+
+#Usefull debugging information
+sqlite3 -version
+
+#Import all data
+python manage.py test
+python manage.py sync_from_webservice
+python manage.py update_affair_details --parallel 12
