f5fd23943d8d422f83b7d43d45ff08a0f84f3981
Reto Schneider <code@reto-schneider.ch>
2016-03-16 23:07:10 +0100

    Fetch data for the new webservice

--- apps/curia_vista/docs/fetch.sh
+++ apps/curia_vista/docs/fetch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e                              #Exit script on error
-cd "$(dirname "$0")"                #Change to script directory
-mkdir -p xml-examples xsd-schemas   #Create subdirectories
+set -e                  #Exit script on error
+cd "$(dirname "$0")"    #Change to script directory
+mkdir -p schemas        #Create subdirectory/ies
 
 fetch()
 {
@@ -17,16 +17,5 @@ fetchXml()
     fetch ${URL} | xmllint --format -
 }
 
-#Fetch XML and XSD documents
-for n in Councils Councillors Schedules Affairs AffairSummaries Committees LegislativePeriods Departments Cantons Factions Sessions Parties Votes; do
-  fetchXml "http://ws-old.parlament.ch/$n?format=xsd" > xsd-schemas/$n.xsd
-  fetchXml "http://ws-old.parlament.ch/$n?format=xml&lang=de" > xml-examples/$n-example.xml
-done
-
-#Fetch XML and XSD files from special URL
-fetchXml "http://ws-old.parlament.ch/Schedules/2015/ALL?format=xsd" > xsd-schemas/Schedules-2015-ALL.xsd
-fetchXml "http://ws-old.parlament.ch/Schedules/2015/ALL?format=xml&lang=de" > xml-examples/Schedules-2015-ALL.xml
-
-#Fetch PDFs
-fetch 'http://www.parlament.ch/d/dokumentation/webservices-opendata/Documents/webservices-info-dritte-d.pdf' > 'webservices-info-dritte-d.pdf'
-fetch 'http://www.parlament.ch/e/dokumentation/webservices-opendata/Documents/webservices-info-dritte-e.pdf' > 'webservices-info-dritte-e.pdf'
+#Fetch XSD files
+fetchXml 'https://ws.parlament.ch/odata.svc/$metadata' > schemas/global.xml
